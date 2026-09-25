29c784165da3af61c91c70fc4505c69cff32bd57
Paul Otto <potto007@gmail.com>
2026-09-25 07:33:08 -0600

    chore: rebuild for Valheim 1.0.16

--- Plugin.cs
+++ Plugin.cs
@@ -24,7 +24,7 @@ namespace OttoAura
     public class OttoAuraPlugin : BaseUnityPlugin
     {
         internal const string ModName = "OttoAura";
-        internal const string ModVersion = "1.4.2";
+        internal const string ModVersion = "1.4.3";
         internal const string Author = "potto007";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
